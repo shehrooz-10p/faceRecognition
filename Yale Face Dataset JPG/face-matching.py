84ea92ad3f27c86acb589f194ee46f46ec4343de
shehrooz sattar <shehrooz.sattar@10pearls.com>
2023-02-24 14:28:52 +0500

    initial commit

---
+++ Yale Face Dataset JPG/face-matching.py
@@ -0,0 +1,227 @@
+## Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+import json
+import sys
+import time
+
+
+class VideoDetect:
+    jobId = ''
+
+    roleArn = ''
+    bucket = ''
+    video = ''
+    startJobId = ''
+
+    sqsQueueUrl = ''
+    snsTopicArn = ''
+    processType = ''
+
+    def __init__(self, role, bucket, video, client, rek, sqs, sns):
+        self.roleArn = role
+        self.bucket = bucket
+        self.video = video
+        self.client = client
+        self.rek = rek
+        self.sqs = sqs
+        self.sns = sns
+
+    def GetSQSMessageSuccess(self):
+
+        jobFound = False
+        succeeded = False
+
+        dotLine = 0
+        while jobFound == False:
+            sqsResponse = self.sqs.receive_message(QueueUrl=self.sqsQueueUrl, MessageAttributeNames=['ALL'],
+                                                   MaxNumberOfMessages=10)
+
+            if sqsResponse:
+
+                if 'Messages' not in sqsResponse:
+                    if dotLine < 40:
+                        print('.', end='')
+                        dotLine = dotLine + 1
+                    else:
+                        print()
+                        dotLine = 0
+                    sys.stdout.flush()
+                    time.sleep(5)
+                    continue
+
+                for message in sqsResponse['Messages']:
+                    notification = json.loads(message['Body'])
+                    rekMessage = json.loads(notification['Message'])
+                    print(rekMessage['JobId'])
+                    print(rekMessage['Status'])
+                    if rekMessage['JobId'] == self.startJobId:
+                        print('Matching Job Found:' + rekMessage['JobId'])
+                        jobFound = True
+                        if (rekMessage['Status'] == 'SUCCEEDED'):
+                            succeeded = True
+
+                        self.sqs.delete_message(QueueUrl=self.sqsQueueUrl,
+                                                ReceiptHandle=message['ReceiptHandle'])
+                    else:
+                        print("Job didn't match:" +
+                              str(rekMessage['JobId']) + ' : ' + self.startJobId)
+                    # Delete the unknown message. Consider sending to dead letter queue
+                    self.sqs.delete_message(QueueUrl=self.sqsQueueUrl,
+                                            ReceiptHandle=message['ReceiptHandle'])
+
+        return succeeded
+
+    def StartLabelDetection(self):
+        response = self.rek.start_label_detection(Video={'S3Object': {'Bucket': self.bucket, 'Name': self.video}},
+                                                  NotificationChannel={'RoleArn': self.roleArn,
+                                                                       'SNSTopicArn': self.snsTopicArn},
+                                                  MinConfidence=90,
+                                                  # Filtration options, uncomment and add desired labels to filter returned labels
+                                                  # Features=['GENERAL_LABELS'],
+                                                  # Settings={
+                                                  # 'GeneralLabels': {
+                                                  # 'LabelInclusionFilters': ['Clothing']
+                                                  # }}
+                                                  )
+
+        self.startJobId = response['JobId']
+        print('Start Job Id: ' + self.startJobId)
+
+    def GetLabelDetectionResults(self):
+        maxResults = 10
+        paginationToken = ''
+        finished = False
+
+        while finished == False:
+            response = self.rek.get_label_detection(JobId=self.startJobId,
+                                                    MaxResults=maxResults,
+                                                    NextToken=paginationToken,
+                                                    SortBy='TIMESTAMP')
+
+            print('Codec: ' + response['VideoMetadata']['Codec'])
+            print('Duration: ' + str(response['VideoMetadata']['DurationMillis']))
+            print('Format: ' + response['VideoMetadata']['Format'])
+            print('Frame rate: ' + str(response['VideoMetadata']['FrameRate']))
+            print()
+
+            for labelDetection in response['Labels']:
+                label = labelDetection['Label']
+                if label['Name'] == 'Adult' or label['Name'] == 'Face':
+                    print("Timestamp: " + str(labelDetection['Timestamp']))
+                    print("   Label: " + label['Name'])
+                    print("   Confidence: " + str(label['Confidence']))
+                    print("   Instances:")
+                    for instance in label['Instances']:
+                        print("      Confidence: " + str(instance['Confidence']))
+                        print("      Bounding box")
+                        print("        Top: " + str(instance['BoundingBox']['Top']))
+                        print("        Left: " + str(instance['BoundingBox']['Left']))
+                        print("        Width: " + str(instance['BoundingBox']['Width']))
+                        print("        Height: " + str(instance['BoundingBox']['Height']))
+                        print()
+                    print()
+
+                    if 'Parents' in label.keys():
+                        print("Parents:")
+                        for parent in label['Parents']:
+                            print("   " + parent['Name'])
+
+                    if 'Aliases' in label.keys():
+                        print("Aliases:")
+                        for alias in label['Aliases']:
+                            print("   " + alias['Name'])
+                    if 'Categories' in label.keys():
+                        print("Categories:")
+                        for category in label['Categories']:
+                            print("   " + category['Name'])
+
+                    print("----------")
+                    print()
+
+                if 'NextToken' in response:
+                    paginationToken = response['NextToken']
+                else:
+                    finished = True
+
+    def CreateTopicandQueue(self):
+
+        millis = str(int(round(time.time() * 1000)))
+
+        # Create SNS topic
+
+        snsTopicName = "AmazonRekognitionExample" + millis
+
+        topicResponse = self.sns.create_topic(Name=snsTopicName)
+        self.snsTopicArn = topicResponse['TopicArn']
+
+        # create SQS queue
+        sqsQueueName = "AmazonRekognitionQueue" + millis
+        self.sqs.create_queue(QueueName=sqsQueueName)
+        self.sqsQueueUrl = self.sqs.get_queue_url(QueueName=sqsQueueName)['QueueUrl']
+
+        attribs = self.sqs.get_queue_attributes(QueueUrl=self.sqsQueueUrl,
+                                                AttributeNames=['QueueArn'])['Attributes']
+
+        sqsQueueArn = attribs['QueueArn']
+
+        # Subscribe SQS queue to SNS topic
+        self.sns.subscribe(
+            TopicArn=self.snsTopicArn,
+            Protocol='sqs',
+            Endpoint=sqsQueueArn)
+
+        # Authorize SNS to write SQS queue
+        policy = """{{
+  "Version":"2012-10-17",
+  "Statement":[
+    {{
+      "Sid":"MyPolicy",
+      "Effect":"Allow",
+      "Principal" : {{"AWS" : "*"}},
+      "Action":"SQS:SendMessage",
+      "Resource": "{}",
+      "Condition":{{
+        "ArnEquals":{{
+          "aws:SourceArn": "{}"
+        }}
+      }}
+    }}
+  ]
+}}""".format(sqsQueueArn, self.snsTopicArn)
+
+        response = self.sqs.set_queue_attributes(
+            QueueUrl=self.sqsQueueUrl,
+            Attributes={
+                'Policy': policy
+            })
+
+    def DeleteTopicandQueue(self):
+        self.sqs.delete_queue(QueueUrl=self.sqsQueueUrl)
+        self.sns.delete_topic(TopicArn=self.snsTopicArn)
+
+
+def main():
+    roleArn = 'arn:aws:iam::051744709538:role/RekognitionRoleToAccessSNS'
+    bucket = 'face-recognition-in-videos'
+    video = 'video3.mp4'
+
+    session = boto3.Session(profile_name='default')
+    client = session.client('rekognition')
+    rek = boto3.client('rekognition')
+    sqs = boto3.client('sqs')
+    sns = boto3.client('sns')
+
+    analyzer = VideoDetect(roleArn, bucket, video, client, rek, sqs, sns)
+    analyzer.CreateTopicandQueue()
+
+    analyzer.StartLabelDetection()
+    if analyzer.GetSQSMessageSuccess() == True:
+        analyzer.GetLabelDetectionResults()
+
+    analyzer.DeleteTopicandQueue()
+
+
+if __name__ == "__main__":
+    main()
